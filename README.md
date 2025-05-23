41fd74eeb7c7e768880b0bfec81069f61c2995e7
André <callejaoandre24@gmail.com>
2025-05-23 19:28:56 -0300

    primeira versão do README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# CP2
+# minfo.sh
+
+Script Bash para exibir informações básicas do sistema.
+
+## Descrição
+
+O `minfo.sh` é um script simples que aceita um argumento para exibir:
+- o nome do host (hostname),
+- o tempo de atividade (uptime),
+- o uso de disco da partição raiz (disk),
+- ou todas as informações de uma vez (all).
+
+## Uso
+
+```bash
+./minfo.sh {hostname|uptime|disk|all}
---
+++ minfo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+    exit 1
+fi
+
+case "$1" in
+    hostname)
+        hostname
+        ;;
+    uptime)
+        uptime -p
+        ;;
+    disk)
+        df -h /
+        ;;
+    all)
+        hostname
+        uptime -p
+        df -h /
+        ;;
+    *)
+        echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+        exit 1
+        ;;
+esac
